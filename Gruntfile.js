313e7d1243a81a22439506149bf1232c3321b280
Sebastian Mahr <sebastian.mahr@sovanta.com>
2016-08-02 14:28:23 +0200

    test

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+/**
+ * Created by sebastian.mahr on 02.08.16.
+ */
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: ["*.*"],
+            reporter: require('jshint-stylish')
+        },
+        copy: {
+            hooks: {
+                expand: true,
+                cwd: "git-hooks",
+                src: "pre-commit",
+                dest: ".git/hooks/"
+            }
+        },
+        clean: {
+            // Clean any pre-commit hooks in .git/hooks directory
+            hooks: ['.git/hooks/pre-commit']
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    ;;;
+    ;;;;;;
+    ;;;;;;
+    ;;;;;
+    ;;;;;
+
+    // Clean the .git/hooks/pre-commit file then copy in the latest version
+    grunt.registerTask('hookmeup', ['clean:hooks', 'copy:hooks']);
+
+};
